40b2b9bdb5f9cc5d519cb453ff191112ded8c6cd
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-19 21:13:40 -0300

    Organizando rotas

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Pages
+import Login from './routes/Login/Login.tsx';
+import Registro from './routes/Registro.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"/",
+        element: <Login />
+      },
+      {
+        path:"/registro",
+        element: <Registro />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
