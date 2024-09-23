2da2ba9987a88760a24c77648ae6c70de757cf72
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-22 22:42:18 -0300

    2-feat-Adicionando uma simulação de login com localStorage

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,7 +1,42 @@
-import { Link } from "react-router-dom";
+import { UserLoginProps } from "../../types/UserLogin";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import './Login.css'
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const [userLogin, setUserLogin] = useState<UserLoginProps>({
+        email: '',
+        senha:''
+    })
+
+    // Função para lidar com a mudança dos campos do formulário
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value, // Atualiza o campo específico
+        });
+    };
+
+    // Função para fazer o login
+    const handleLogin = () => {
+        const savedUser = localStorage.getItem('user');
+        if (savedUser) {
+            const user = JSON.parse(savedUser);
+
+            // Verifica se o email e a senha correspondem
+            if (user.email === userLogin.email && user.senha === userLogin.senha) {
+                alert('Login bem-sucedido!');
+                navigate('/task'); // Redireciona para a página principal (exemplo)
+            } else {
+                alert('Email ou senha incorretos!');
+            }
+        } else {
+            alert('Usuário não encontrado! Por favor, registre-se.');
+        }
+    };
+
     return (
         <div className="area-login">
             <div className="area-texto-login">
@@ -10,14 +45,27 @@ const Login = () => {
             </div>
             <div className="area-caixa-formulario">
                 <div className="campo-formulario">
-                    <input type="text" placeholder="Email" id="campo-email"/>
+                    <input 
+                        type="text" 
+                        name="email"
+                        placeholder="Email" 
+                        id="campo-email"
+                        value={userLogin.email}
+                        onChange={handleChange}
+                    />
                 </div>
                 <div className="campo-formulario">
-                    <input type="password" placeholder="Senha"/>
+                    <input 
+                        type="password" 
+                        name="senha"
+                        placeholder="Senha"
+                        value={userLogin.senha}
+                        onChange={handleChange}
+                    />
                 </div>
                 
                 <p id="alterar-senha">Esqueci a senha</p>
-                <button>Entrar</button>
+                <button onClick={handleLogin}>Entrar</button>
             </div>
             <div className="area-registrar">
                 <p>
